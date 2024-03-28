7330e9d2ad865d8babfc7c91a9400ce660bc4cc1
sarim nadeem <115877996+samcodeonline@users.noreply.github.com>
2024-03-28 18:24:40 +0500

    change

--- FunctionalProg(java)/StudentResultCardGenerator/src/Main.java
+++ FunctionalProg(java)/StudentResultCardGenerator/src/Main.java
@@ -1,27 +1,56 @@
 import java.util.Scanner;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-            int studentCount, subjectCount ;
+            Scanner s = new Scanner(System.in);
 
             System.out.print("No of Students: ");
             Scanner totalStudent = new Scanner(System.in);
+            int studentCount;
             studentCount = totalStudent.nextInt();
-
             String[] studentNames = new String [studentCount];
+            System.out.print ("No of Subjects: ");
+            int subjectCount;
+            Scanner totalSubjects = new Scanner(System.in);
+            subjectCount = totalSubjects.nextInt();
+            String[] subjectNames = new String[subjectCount];
 
-            System.out.println(  studentNames.length);
-            System.out.print("No of Subjects: ");
-            Scanner totalSubject = new Scanner(System.in);
-            subjectCount = totalSubject.nextInt();
-
-            String[] nameOfSubject = new String [subjectCount];
-
+            String[] nameOfSubject = new String[subjectCount];
             System.out.println("Enter Subject Names: ");
-            for(int printSubjectName = 0 ; printSubjectName <= nameOfSubject.length ; printSubjectName++) {
-                    System.out.println("Subject " + printSubjectName + 1 + " :");
-                    Scanner subjectNames = new Scanner(System.in);
-                    String subject_Names = subjectNames.nextLine();
-                    nameOfSubject[printSubjectName] = subject_Names;
+            for(int priSubName = 0 ; priSubName < subjectNames.length; priSubName++){
+                    System.out.println("Subject" + priSubName + " :");
+                    nameOfSubject[priSubName] = s.next();
             }
+
+            int marks[][] = new int[studentCount][subjectCount];
+            for(int students = 0 ; students < studentNames.length; students++){
+                    System.out.print ("\n") ;
+                    System.out.print ("Student Name" + ": ");
+                    studentNames[students] = s.next();
+                    for (int subjects = 0 ; subjects < subjectNames.length; subjects++){
+
+                            System.out.println("Subject" + subjects+1 + " Marks: ");
+                            marks[students][subjects] = s.nextInt();
+                    }
+            }
+
+            System.out.println("Name\t");
+            for(int tableHeader = 0 ; tableHeader < subjectCount ; tableHeader++){
+                    System.out.print(nameOfSubject[tableHeader] + "\t");
+            }
+
+            System.out.println("Obtained\tTotal\tPercent\tGrade");
+
+            for(int students = 0; students < studentNames.length; students++){
+                    System.out.print ("\n");
+                    System.out.print (studentNames[students] + "\t");
+                    double obtainedMarks = 0;
+                    for (int subjects = 0; subjects < subjectNames.length; subjects++){
+
+                            System.out.print (marks[students][subjects] + "\t");
+                            obtainedMarks += marks[students][subjects];
+                    }
+            }
+
     }
 }
--- IntelliJ(intro)/helloWorld/src/Main.java
+++ IntelliJ(intro)/helloWorld/src/Main.java
@@ -2,11 +2,24 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-//            int studentCount = 0;
-        System.out.print("How many students: ");
-        Scanner student_count = new Scanner(System.in);
-        int total_student = student_count.nextInt();
-        int[] marks = new int[total_student];
+            int studentCount = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the total elements to sort");
+        Scanner inputCount = new Scanner(System.in);
+        studentCount = inputCount.nextInt();
+        String[] array = new String[studentCount];
+
+        System.out.println("Please enter names to sort");
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = s.nextLine();
+        }
+        for(int i = 0 ; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+
+        //Just to test
+//        System.out.println(array[0]);
     }
 }
 
