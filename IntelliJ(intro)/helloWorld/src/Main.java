50399acc102dbe960a1508e4200696dca28206e0
sarim nadeem <115877996+samcodeonline@users.noreply.github.com>
2024-04-01 17:50:41 +0500

    change

--- IntelliJ(intro)/helloWorld/src/Main.java
+++ IntelliJ(intro)/helloWorld/src/Main.java
@@ -1,25 +1,12 @@
 import java.util.*;
-
+import java.util.LinkedList;
 class Main {
     public static void main(String[] args) {
-            int studentCount = 0;
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the total elements to sort");
-        Scanner inputCount = new Scanner(System.in);
-        studentCount = inputCount.nextInt();
-        String[] array = new String[studentCount];
-
-        System.out.println("Please enter names to sort");
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = s.nextLine();
-        }
-        for(int i = 0 ; i < array.length; i++){
-            System.out.println(array[i]);
-        }
-
-        //Just to test
-//        System.out.println(array[0]);
+            LinkedList<String> nameOfStudents = new LinkedList<String>();
+             nameOfStudents.add("sam");
+             nameOfStudents.add("codeonline");
+             nameOfStudents.add("rapanzal");
+        System.out.println(nameOfStudents);
     }
 }
 
